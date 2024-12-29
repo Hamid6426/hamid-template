a71495ba26f9acb0232a1b9dcb853c69d62a44f1
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2024-12-29 15:10:07 +0500

    Initial commit

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
+import path from "path";
+
+const app = express();
+dotenv.config()
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+const PORT = process.env.PORT;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
